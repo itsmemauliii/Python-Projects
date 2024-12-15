892698337bdbf6fe2f63f158c4fee7de58db1e98
itsmemauli <124795820+itsmemauliii@users.noreply.github.com>
2024-12-15 14:03:00 +0530

    Create challenge3_snapple_facts.py

---
+++ challenge3_snapple_facts.py
@@ -0,0 +1,14 @@
+import random
+fact_number = random.randint(0, 5)
+if fact_number == 0:
+    print("Flamingos turn pink from eating shrimp.")
+elif fact_number == 1:
+    print("The only food that doesn't spoil is honey.")
+elif fact_number == 2:
+    print("Shrimp can only swim backwards.")
+elif fact_number == 3:
+    print("A taste bud's life span is about 10 days.")
+elif fact_number == 4:
+    print("It is impossible to sneeze while sleeping.")
+elif fact_number == 5:
+    print("It is illegal to sing off-key in North Carolina.")
