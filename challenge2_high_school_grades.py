4efead1115b02d2cea2234db32c98663db14aba5
itsmemauli <124795820+itsmemauliii@users.noreply.github.com>
2024-12-15 14:02:14 +0530

    Create challenge2_high_school_grades.py

---
+++ challenge2_high_school_grades.py
@@ -0,0 +1,11 @@
+grade = int(input("Enter your grade (9-12): "))
+if grade == 9:
+    print("Freshman")
+elif grade == 10:
+    print("Sophomore")
+elif grade == 11:
+    print("Junior")
+elif grade == 12:
+    print("Senior")
+else:
+    print("TBD")
