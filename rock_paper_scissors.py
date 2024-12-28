51369a18cbfb5f4e14dac7724dbe30d8d2976718
itsmemauli <124795820+itsmemauliii@users.noreply.github.com>
2024-12-28 15:43:27 +0530

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,9 +1,11 @@
 from random import randint
+
 t = ["Rock", "Paper", "Scissors"]
-computer = t[randint(0,2)]
+computer = t[randint(0, 2)]
 player = False
+
 while player == False:
-    player = input("Rock, Paper, Scissors?")
+    player = input("Rock, Paper, Scissors? ")
     if player == computer:
         print("Tie!")
     elif player == "Rock":
@@ -11,3 +13,17 @@ while player == False:
             print("You lose!", computer, "covers", player)
         else:
             print("You win!", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cuts", player)
+        else:
+            print("You win!", player, "covers", computer)
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose!", computer, "smashes", player)
+        else:
+            print("You win!", player, "cuts", computer)
+    else:
+        print("Invalid input. Please choose Rock, Paper, or Scissors.")
+    player = False
+    computer = t[randint(0, 2)]
